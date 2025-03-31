6e05010c33a07e9292c9f066f9d5644b78b3caa1
Oscar Daniel <oscardguzman92@gmail.com>
2025-03-30 23:24:42 -0500

    Docker container

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -7,7 +7,7 @@ import { Client } from '../models/client.model';
   providedIn: 'root'
 })
 export class ClientService {
-  private apiUrl = 'http://localhost:8080/clients'
+  private apiUrl = 'api/clients'
   
   constructor(private http: HttpClient) { }
 
